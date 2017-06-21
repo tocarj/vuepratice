10a0dc189d0bbf50175f431addcee1074397e562
Bruno Rodrigues Soares <bsoares@medbarra.com.br>
2017-06-20 22:20:09 -0300

    testes iniciais

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'Você carregou esta página às' + new Date(),
+        todos:[
+        {id:0, text: 'Aprender Vue'},
+        {id:1, text: 'Aprender Python'},
+        {id:2, text: 'Aprender Docker'}]
+    },
+    methods:{
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+
+        }
+
+    }
+
+});
